b51a930359254cb039fdce84a7889e650edb7b89
hodolee246 <hodolee246@naver.com>
2021-09-10 17:36:55 +0900

    ch1 11장 완료

--- src/main/java/com/example/tdd/ch1/Dollar.java
+++
@@ -1,9 +0,0 @@
-package com.example.tdd.ch1;
-
-public class Dollar extends Money {
-
-    public Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-
-}
--- src/main/java/com/example/tdd/ch1/Franc.java
+++
@@ -1,9 +0,0 @@
-package com.example.tdd.ch1;
-
-public class Franc extends Money {
-
-    public Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-
-}
